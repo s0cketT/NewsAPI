8c50d134ab1ab2beaa44df7c4f6366871f12760e
s0cketT <137925156+s0cketT@users.noreply.github.com>
2025-01-26 11:57:29 +0300

    First commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,27 +44,25 @@ android {
 
 dependencies {
     //UI
-    implementation("androidx.compose.material:material-icons-extended-android:1.5.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation(libs.androidx.material.icons.extended.android)
+    implementation(libs.coil.compose)
 
     //Network
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     //DI
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
     //Navigation
-    val nav_version = "2.8.1"
-    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation(libs.androidx.navigation.compose)
 
     //Room
-    val room_version = "2.6.1"
-    implementation("androidx.room:room-runtime:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
